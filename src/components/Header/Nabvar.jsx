4b67830848b6044cd85e97c1965de85f5a938ac7
ReshadSadik <reshadsadik@gmail.com>
2023-02-15 19:38:47 +0600

    added theme customization

--- src/components/Header/Nabvar.jsx
+++ src/components/Header/Nabvar.jsx
@@ -5,10 +5,12 @@ import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import DrawerSmall from './DrawerSmall';
 import DrawerLarge from './DrawerLarge';
+import { useTheme } from '@mui/material/styles';
 const drawerWidth = 240;
 export const navItems = ['Home', 'About', 'Contact'];
 
 function Nabvar(props) {
+  const theme = useTheme();
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -42,7 +44,7 @@ function Nabvar(props) {
         </Drawer>
       </Box>
       <Box component="main" sx={{ p: 3 }}>
-        <Typography>
+        <Typography color={theme.palette.text.danger}>
           main content there
         </Typography>
       </Box>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
-
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main : "#1976d2"
+    },
+    text: {
+      success: "#00FF00",
+      danger :"#FF0000"
+    }
+ }
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+     <ThemeProvider theme={theme}>
+       < Component {...pageProps } />
+     </ThemeProvider>
+  )
 }
